a3b308e56d57ba5facaa2ec8aa0f68547bd1ec0a
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-07 09:28:00 +1300

    add footer and font awesome

--- client/app/components/footer/footer.component.ts
+++ client/app/components/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'footer',
-  templateUrl: 'footer.component.html'
+  templateUrl: 'footer.component.html',
+  styleUrls: ['footer.component.css']
 })
 export class FooterComponent  { }
