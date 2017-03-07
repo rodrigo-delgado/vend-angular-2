14152841604787c057be88d0bb7b2acb86489d9b
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-08 12:09:44 +1300

    add heroku configuration

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
+//this is already setup for heroku
 var express = require('express')
 var path = require('path')
-var bodyParser = require('body-parser')
+
+
+//Create an App
+
 
 var index = require('./routes')
 
-var port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 var app = express()
 
-//View engine
-app.set('views', path.join(__dirname, 'views'))
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url)
+
+  } else {
+    next()
+  }
+})
+app.use(express.static(path.join(__dirname, 'client')))
 app.set('view engine', 'ejs')
 app.engine('html',require('ejs').renderFile)
 
 //Static folder
-app.use(express.static(path.join(__dirname, 'client')))
 
-//Body Parser MW
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extend: false}))
+app.use(express.static('public'))
 
 app.use('/', index)
 
-app.listen(port, () => {
-  console.log(`server started on on port ${port}`)
+app.listen(PORT, function(){
+  console.log('Express server is running on port ' + PORT)
 })
